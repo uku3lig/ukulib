3530f505eb47ecd65d3fc80bac74dbbac54b2108
uku <uku3lig@gmail.com>
2022-10-11 21:13:04 +0200

    fix invisible button

--- src/main/java/net/uku3lig/ukulib/config/screen/AbstractConfigScreen.java
+++ src/main/java/net/uku3lig/ukulib/config/screen/AbstractConfigScreen.java
@@ -60,7 +60,7 @@ public abstract class AbstractConfigScreen<T extends IConfig<T>> extends GameOpt
      * Draws the buttons in the footer.
      */
     protected void drawFooterButtons() {
-        this.children.add(new ButtonWidget(this.width / 2 - 100, this.height - 27, 200, 20, I18n.translate("gui.done"),
+        this.addButton(new ButtonWidget(this.width / 2 - 100, this.height - 27, 200, 20, I18n.translate("gui.done"),
                 button -> MinecraftClient.getInstance().openScreen(this.parent)));
     }
 
