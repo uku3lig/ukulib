086e6aa2013dfca539e4917a18b420e3324b54e6
uku <uku3lig@gmail.com>
2022-10-14 14:26:27 +0200

    add shinier uku button in options screen

--- src/main/java/net/uku3lig/ukulib/mixin/MixinOptionsScreen.java
+++ src/main/java/net/uku3lig/ukulib/mixin/MixinOptionsScreen.java
@@ -3,9 +3,10 @@ package net.uku3lig.ukulib.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.uku3lig.ukulib.config.screen.UkulibConfigScreen;
+import net.uku3lig.ukulib.utils.IconButton;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,10 +14,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(OptionsScreen.class)
 public class MixinOptionsScreen extends Screen {
+    private static final Identifier ICON = new Identifier("ukulib", "uku.png");
+
     @Inject(method = "init", at = @At("RETURN"))
     public void addBetterHurtCamButton(CallbackInfo ci) {
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 155, this.height / 6 + 24 - 6, 150, 20,
-                Text.of("Ukulib Mods..."), button -> MinecraftClient.getInstance().setScreen(new UkulibConfigScreen(this))));
+        this.addDrawableChild(new IconButton(this.width / 2 + 158, this.height / 6 + 120 - 6, 20, 20, 0, 0, 20, ICON, 20, 20, button -> MinecraftClient.getInstance().setScreen(new UkulibConfigScreen(this))));
     }
 
     protected MixinOptionsScreen(Text title) {
---
+++ src/main/java/net/uku3lig/ukulib/utils/IconButton.java
@@ -0,0 +1,29 @@
+package net.uku3lig.ukulib.utils;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+
+public class IconButton extends TexturedButtonWidget {
+    public IconButton(int x, int y, int width, int height, int u, int v, int hoveredVOffset, Identifier texture, int textureWidth, int textureHeight, PressAction pressAction) {
+        super(x, y, width, height, u, v, hoveredVOffset, texture, textureWidth, textureHeight, pressAction);
+    }
+
+    @Override
+    public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        // i love stealing minecraft's code
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, WIDGETS_TEXTURE);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, this.alpha);
+        int i = this.getYImage(this.isHovered());
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.enableDepthTest();
+        this.drawTexture(matrices, this.x, this.y, 0, 46 + i * 20, this.width / 2, this.height);
+        this.drawTexture(matrices, this.x + this.width / 2, this.y, 200 - this.width / 2, 46 + i * 20, this.width / 2, this.height);
+
+        super.renderButton(matrices, mouseX, mouseY, delta);
+    }
+}
