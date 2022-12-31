1bd2a0baa681cf9a78970e7f4e7fd1fcb6db4014
uku <uku3lig@gmail.com>
2022-12-31 18:21:50 +0100

    backport 0.6.1 to 1.18.2

--- src/main/java/net/uku3lig/ukulib/utils/PlayerArgumentType.java
+++ src/main/java/net/uku3lig/ukulib/utils/PlayerArgumentType.java
@@ -7,10 +7,10 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
 import net.minecraft.command.CommandSource;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -23,7 +23,7 @@ public class PlayerArgumentType implements ArgumentType<PlayerArgumentType.Playe
     /**
      * The exception thrown when the selected player is not found.
      */
-    public static final SimpleCommandExceptionType PLAYER_NOT_FOUND_EXCEPTION = new SimpleCommandExceptionType(Text.translatable("name.entity.notfound.player"));
+    public static final SimpleCommandExceptionType PLAYER_NOT_FOUND_EXCEPTION = new SimpleCommandExceptionType(new TranslatableText("name.entity.notfound.player"));
 
     /**
      * Returns a new instance.
