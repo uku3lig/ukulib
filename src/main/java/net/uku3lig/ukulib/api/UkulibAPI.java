c1199dff2aef4865e70d8868cc59f16f1ac0a6de
uku <uku3lig@gmail.com>
2023-03-14 16:57:14 +0100

    return UnaryOperator in UkulibAPI

--- src/main/java/net/uku3lig/ukulib/api/UkulibAPI.java
+++ src/main/java/net/uku3lig/ukulib/api/UkulibAPI.java
@@ -1,9 +1,8 @@
 package net.uku3lig.ukulib.api;
 
 import net.minecraft.client.gui.screen.Screen;
-import net.uku3lig.ukulib.config.screen.AbstractConfigScreen;
 
-import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 /**
  * This interface is used to integrate your mod with Ukulib.
@@ -11,9 +10,10 @@ import java.util.function.Function;
 public interface UkulibAPI {
     /**
      * This method is used to show your mod's config screen in the Ukulib config screen.
+     * The lambda parameter is the parent screen.
      * @return A function that takes a parent screen and returns a config screen.
      */
-    Function<Screen, AbstractConfigScreen<?>> supplyConfigScreen();
+    UnaryOperator<Screen> supplyConfigScreen();
 
     /**
      * This method is used to enable or disable the integration with Mod Menu.
