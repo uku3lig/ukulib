5ee0e50c4ece54c3ed323734b54d907fe166d0b9
uku <uku3lig@gmail.com>
2023-03-17 23:35:48 +0100

    add AutoSavedConfigScreen

---
+++ src/main/java/net/uku3lig/ukulib/config/screen/AutoSavedConfigScreen.java
@@ -0,0 +1,26 @@
+package net.uku3lig.ukulib.config.screen;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+import net.uku3lig.ukulib.config.ConfigManager;
+import net.uku3lig.ukulib.config.IConfig;
+
+public abstract class AutoSavedConfigScreen<T extends IConfig<T>> extends CloseableScreen {
+    protected final ConfigManager<T> manager;
+
+    /**
+     * Constructs a closeable screen
+     *
+     * @param title  The title of the screen
+     * @param parent The parent screen
+     */
+    protected AutoSavedConfigScreen(Text title, Screen parent, ConfigManager<T> manager) {
+        super(title, parent);
+        this.manager = manager;
+    }
+
+    @Override
+    public void removed() {
+        manager.saveConfig();
+    }
+}
