06220d8e873d1f79cd010870b36596f2e44650c1
uku <uku3lig@gmail.com>
2023-03-18 00:35:53 +0100

    use new option system in AbstractConfigScreen

--- src/main/java/net/uku3lig/ukulib/config/screen/AbstractConfigScreen.java
+++ src/main/java/net/uku3lig/ukulib/config/screen/AbstractConfigScreen.java
@@ -5,12 +5,13 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
-import net.minecraft.client.gui.widget.OptionListWidget;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.text.Text;
 import net.uku3lig.ukulib.config.ConfigManager;
 import net.uku3lig.ukulib.config.IConfig;
 import net.uku3lig.ukulib.config.impl.BrokenConfigScreen;
+import net.uku3lig.ukulib.config.option.ButtonCreator;
+import net.uku3lig.ukulib.config.option.widget.ButtonCreatorList;
 import net.uku3lig.ukulib.utils.Ukutils;
 
 /**
@@ -30,7 +31,7 @@ public abstract class AbstractConfigScreen<T extends IConfig<T>> extends GameOpt
      * The widget used to display the options.
      * @see AbstractConfigScreen#getOptions(IConfig)
      */
-    protected OptionListWidget optionList;
+    protected ButtonCreatorList buttonList;
 
     /**
      * Creates a config screen.
@@ -48,27 +49,27 @@ public abstract class AbstractConfigScreen<T extends IConfig<T>> extends GameOpt
      * @param config The config
      * @return An array of {@link SimpleOption}
      */
-    protected abstract SimpleOption<?>[] getOptions(T config);
+    protected abstract ButtonCreator[] getOptions(T config);
 
     @Override
     protected void init() {
         super.init();
-        optionList = new OptionListWidget(this.client, this.width, this.height, 32, this.height - 32, 25);
+        buttonList = new ButtonCreatorList(this.client, this.width, this.height, 32, this.height - 32, 25);
 
         try {
-            optionList.addAll(getOptions(manager.getConfig()));
+            buttonList.addAll(getOptions(manager.getConfig()));
         } catch (Exception e) {
             log.error("Error while getting options, replacing config with the default one", e);
             manager.replaceConfig(manager.getConfig().defaultConfig());
             try {
-                optionList.addAll(getOptions(manager.getConfig()));
+                buttonList.addAll(getOptions(manager.getConfig()));
             } catch (Exception e2) {
                 log.error("Error while getting options with the default config, this is a bug", e2);
                 MinecraftClient.getInstance().setScreen(new BrokenConfigScreen(parent));
             }
         }
 
-        this.addSelectableChild(optionList);
+        this.addSelectableChild(buttonList);
         drawFooterButtons();
     }
 
@@ -81,7 +82,10 @@ public abstract class AbstractConfigScreen<T extends IConfig<T>> extends GameOpt
 
     @Override
     public void render(DrawContext drawContext, int mouseX, int mouseY, float delta) {
-        render(drawContext, optionList, mouseX, mouseY, delta);
+        this.renderBackground(drawContext);
+        buttonList.render(drawContext, mouseX, mouseY, delta);
+        drawContext.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
+        super.render(drawContext, mouseX, mouseY, delta);
     }
 
     @Override
