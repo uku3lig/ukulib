7d8bffdc44a4df2e6e8e2ceae9c8ff96aeff51cf
uku <uku3lig@gmail.com>
2023-03-22 15:00:30 +0100

    add done button to BaseConfigScreen

--- src/main/java/net/uku3lig/ukulib/config/screen/BaseConfigScreen.java
+++ src/main/java/net/uku3lig/ukulib/config/screen/BaseConfigScreen.java
@@ -4,6 +4,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 import net.uku3lig.ukulib.config.ConfigManager;
 import net.uku3lig.ukulib.config.IConfig;
+import net.uku3lig.ukulib.utils.Ukutils;
 
 public abstract class BaseConfigScreen<T extends IConfig<T>> extends CloseableScreen {
     protected final ConfigManager<T> manager;
@@ -17,6 +18,11 @@ public abstract class BaseConfigScreen<T extends IConfig<T>> extends CloseableSc
     protected BaseConfigScreen(Text title, Screen parent, ConfigManager<T> manager) {
         super(title, parent);
         this.manager = manager;
+    }
+
+    @Override
+    protected void init() {
+        this.addDrawableChild(Ukutils.doneButton(this.width, this.height, this.parent));
     }
 
     @Override
