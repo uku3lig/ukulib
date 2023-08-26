b66075b49c7a39d289598d74654fa73adf445142
uku <uku3lig@gmail.com>
2023-08-26 23:54:17 +0200

    exclude ukulib config from javadoc checks

--- src/main/java/net/uku3lig/ukulib/config/impl/UkulibConfig.java
+++ src/main/java/net/uku3lig/ukulib/config/impl/UkulibConfig.java
@@ -1,6 +1,8 @@
 package net.uku3lig.ukulib.config.impl;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import net.uku3lig.ukulib.config.ConfigManager;
 
@@ -11,12 +13,9 @@ import java.io.Serializable;
  */
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UkulibConfig implements Serializable {
-    /**
-     * The config manager for this config
-     *
-     * @return The config manager
-     */
     @Getter
     private static final ConfigManager<UkulibConfig> manager = ConfigManager.createDefault(UkulibConfig.class, "ukulib");
 
@@ -31,35 +30,11 @@ public class UkulibConfig implements Serializable {
 
     /**
      * Whether to show the button in the options menu
-     *
-     * @param buttonInOptions the new value
-     * @return the current value
      */
     private boolean buttonInOptions = true;
 
     /**
      * Whether to enable the automatic mod menu integration
-     *
-     * @param modMenuIntegration the new value
-     * @return the current value
      */
     private boolean modMenuIntegration = true;
-
-    /**
-     * Constructs the config
-     */
-    public UkulibConfig() {
-        // default config
-    }
-
-    /**
-     * Constructs the config
-     *
-     * @param buttonInOptions    Whether to show the button in the options menu
-     * @param modMenuIntegration Whether to enable the automatic mod menu integration
-     */
-    public UkulibConfig(boolean buttonInOptions, boolean modMenuIntegration) {
-        this.buttonInOptions = buttonInOptions;
-        this.modMenuIntegration = modMenuIntegration;
-    }
 }
