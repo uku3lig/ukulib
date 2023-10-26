74aa7e7602c2189a9828d4fe92ec3a558f56ee0c
uku <uku3lig@gmail.com>
2023-10-26 22:34:27 +0200

    move registerHeadTex into MixinOptionsScreen

--- src/main/java/net/uku3lig/ukulib/config/impl/UkulibConfigScreen.java
+++ src/main/java/net/uku3lig/ukulib/config/impl/UkulibConfigScreen.java
@@ -1,40 +1,20 @@
 package net.uku3lig.ukulib.config.impl;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.texture.NativeImage;
-import net.minecraft.client.texture.NativeImageBackedTexture;
-import net.minecraft.client.texture.TextureManager;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 import net.uku3lig.ukulib.config.option.CyclingOption;
 import net.uku3lig.ukulib.config.option.InputOption;
 import net.uku3lig.ukulib.config.option.WidgetCreator;
 import net.uku3lig.ukulib.config.screen.AbstractConfigScreen;
 import net.uku3lig.ukulib.utils.Ukutils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.NoSuchElementException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
-
 /**
  * ukulib's config screen.
  */
 @Slf4j
 public class UkulibConfigScreen extends AbstractConfigScreen<UkulibConfig> {
-    private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
-
     /**
      * Creates a config screen.
      *
@@ -56,56 +36,9 @@ public class UkulibConfigScreen extends AbstractConfigScreen<UkulibConfig> {
     @Override
     public void removed() {
         super.removed();
-        if (FabricLoader.getInstance().isModLoaded("vulkanmod")) {
+        if (FabricLoader.getInstance().isModLoaded("vulkanmod") && this.manager.getConfig().getHeadName().equalsIgnoreCase("uku3lig")) {
             log.warn("VulkanMod detected, disabling custom heads.");
-            if (this.manager.getConfig().getHeadName().equalsIgnoreCase("uku3lig")) {
-                Ukutils.sendToast(Text.of("Warning!"), Text.of("Custom heads do not work with VulkanMod."));
-            }
-        } else {
-            registerHeadTex(UkulibConfig.get().getHeadName());
+            Ukutils.sendToast(Text.of("Warning!"), Text.of("Custom heads do not work with VulkanMod."));
         }
-    }
-
-    /**
-     * <h4>WARNING: INTERNAL METHOD, DO NOT USE !!!!!!!!</h4>
-     * Registers the head texture of a player.
-     *
-     * @param username the username of the player
-     * @return a future that completes when the texture is registered
-     */
-    public static CompletableFuture<Void> registerHeadTex(String username) {
-        Identifier texture = Ukutils.getHeadTex(username);
-        if (Ukutils.textureExists(texture)) {
-            return CompletableFuture.completedFuture(null);
-        }
-
-        HttpRequest uuidReq = HttpRequest.newBuilder(URI.create("https://api.mojang.com/users/profiles/minecraft/" + username))
-                .GET().build();
-
-        return HTTP_CLIENT.sendAsync(uuidReq, HttpResponse.BodyHandlers.ofString())
-                .thenAccept(res -> {
-                    if (res.statusCode() != 200) {
-                        log.error("Error while getting UUID of " + username + ": " + res.statusCode() + " " + res.body());
-                        throw new CompletionException(new NoSuchElementException("Error while getting UUID of " + username));
-                    }
-
-                    JsonObject json = new Gson().fromJson(res.body(), JsonObject.class);
-                    String uuid = json.get("id").getAsString();
-
-                    TextureManager texManager = MinecraftClient.getInstance().getTextureManager();
-
-                    if (!Ukutils.textureExists(texture)) {
-                        try {
-                            URL url = new URL("https://crafatar.com/avatars/" + uuid + "?overlay&size=16");
-                            try (InputStream stream = url.openStream()) {
-                                NativeImage image = NativeImage.read(stream);
-                                texManager.registerTexture(texture, new NativeImageBackedTexture(image));
-                            }
-                        } catch (IOException e) {
-                            log.error("Could not fetch head texture", e);
-                            throw new CompletionException(e);
-                        }
-                    }
-                });
     }
 }
--- src/main/java/net/uku3lig/ukulib/mixin/MixinOptionsScreen.java
+++ src/main/java/net/uku3lig/ukulib/mixin/MixinOptionsScreen.java
@@ -1,15 +1,20 @@
 package net.uku3lig.ukulib.mixin;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.NativeImageBackedTexture;
+import net.minecraft.client.texture.TextureManager;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.uku3lig.ukulib.api.UkulibAPI;
 import net.uku3lig.ukulib.config.impl.ModListScreen;
 import net.uku3lig.ukulib.config.impl.UkulibConfig;
-import net.uku3lig.ukulib.config.impl.UkulibConfigScreen;
 import net.uku3lig.ukulib.utils.IconButton;
 import net.uku3lig.ukulib.utils.Ukutils;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,13 +23,28 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.NoSuchElementException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
 /**
  * Mixin for {@link OptionsScreen}.
  */
+@Slf4j
 @Mixin(OptionsScreen.class)
 public class MixinOptionsScreen extends Screen {
     @Unique
     private static final Identifier DEFAULT_ICON = new Identifier("ukulib", "uku.png");
+
+    @Unique
+    private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
 
     @Unique
     private IconButton ukulibButton;
@@ -49,13 +69,57 @@ public class MixinOptionsScreen extends Screen {
             if (Ukutils.textureExists(customHeadTex)) {
                 texture = customHeadTex;
             } else {
-                UkulibConfigScreen.registerHeadTex(username).thenRun(() -> this.ukulibButton.setTexture(customHeadTex));
+                registerHeadTex(username).thenRun(() -> this.ukulibButton.setTexture(customHeadTex));
             }
         }
 
         this.ukulibButton = this.addDrawableChild(new IconButton(this.width / 2 + 158, this.height / 6 + 144 - 6, 20, 20,
                 texture, 16, 16,
                 button -> MinecraftClient.getInstance().setScreen(new ModListScreen(this))));
+    }
+
+    /**
+     * <h4>WARNING: INTERNAL METHOD, DO NOT USE !!!!!!!!</h4>
+     * Registers the head texture of a player.
+     *
+     * @param username the username of the player
+     * @return a future that completes when the texture is registered
+     */
+    @Unique
+    private static CompletableFuture<Void> registerHeadTex(String username) {
+        Identifier texture = Ukutils.getHeadTex(username);
+        if (Ukutils.textureExists(texture)) {
+            return CompletableFuture.completedFuture(null);
+        }
+
+        HttpRequest uuidReq = HttpRequest.newBuilder(URI.create("https://api.mojang.com/users/profiles/minecraft/" + username))
+                .GET().build();
+
+        return HTTP_CLIENT.sendAsync(uuidReq, HttpResponse.BodyHandlers.ofString())
+                .thenAccept(res -> {
+                    if (res.statusCode() != 200) {
+                        log.error("Error while getting UUID of " + username + ": " + res.statusCode() + " " + res.body());
+                        throw new CompletionException(new NoSuchElementException("Error while getting UUID of " + username));
+                    }
+
+                    JsonObject json = new Gson().fromJson(res.body(), JsonObject.class);
+                    String uuid = json.get("id").getAsString();
+
+                    TextureManager texManager = MinecraftClient.getInstance().getTextureManager();
+
+                    if (!Ukutils.textureExists(texture)) {
+                        try {
+                            URL url = new URL("https://crafatar.com/avatars/" + uuid + "?overlay&size=16");
+                            try (InputStream stream = url.openStream()) {
+                                NativeImage image = NativeImage.read(stream);
+                                texManager.registerTexture(texture, new NativeImageBackedTexture(image));
+                            }
+                        } catch (IOException e) {
+                            log.error("Could not fetch head texture", e);
+                            throw new CompletionException(e);
+                        }
+                    }
+                });
     }
 
     /**
