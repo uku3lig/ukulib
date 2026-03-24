5dd12818a5dc6e6cb07fed43f1149a01ea568034
uku <hi@uku.moe>
2026-03-24 11:29:23 +0100

    chore(deps): update gradle plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.modrinth.minotaur:com.modrinth.minotaur.gradle.plugin:2.8.10")
+    implementation("com.modrinth.minotaur:com.modrinth.minotaur.gradle.plugin:2.9.0")
     implementation("org.ow2.asm:asm:9.9.1")
 }
