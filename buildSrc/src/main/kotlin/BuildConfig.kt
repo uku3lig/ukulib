3c28d8a6fd9629b2618f4eb0fb9cf0d7fc1411a8
uku <hi@uku.moe>
2026-06-16 18:17:58 +0200

    feat: 26.2

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,10 +1,10 @@
 import org.gradle.api.Project
 
 object BuildConfig {
-    const val MINECRAFT_VERSION: String = "26.2-rc-2"
+    const val MINECRAFT_VERSION: String = "26.2"
     const val FABRIC_LOADER_VERSION: String = "0.19.3"
-    const val FABRIC_API_VERSION: String = "0.152.0+26.2"
-    const val NEOFORGE_VERSION: String = "26.2.0-alpha.0+rc-2.20260614.052634"
+    const val FABRIC_API_VERSION: String = "0.152.1+26.2"
+    const val NEOFORGE_VERSION: String = "26.2.0.0-beta"
 
     const val TOML4J_VERSION: String = "0.7.2"
     const val MODMENU_VERSION: String = "20.0.0-beta.2"
