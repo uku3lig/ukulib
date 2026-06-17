419a2dd8d14f24f3bba47fda9963779e7a599810
uku <hi@uku.moe>
2026-06-17 20:14:42 +0200

    chore: bump version to 2.1.1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -9,7 +9,7 @@ object BuildConfig {
     const val TOML4J_VERSION: String = "0.7.2"
     const val MODMENU_VERSION: String = "20.0.0-beta.2"
 
-    const val MOD_VERSION: String = "2.1.0"
+    const val MOD_VERSION: String = "2.1.1"
 
     const val MODRINTH_PROJECT_ID: String = "Y8uFrUil"
 
